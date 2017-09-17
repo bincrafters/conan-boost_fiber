e5eebfde4715cfb01c05ae270460849737a6d080
Jerry Wiltse <jerrywiltse@gmail.com>
2017-09-17 11:30:11 -0400

    Updated version to 1.65.1

--- conanfile.py
+++ conanfile.py
@@ -3,24 +3,24 @@ from conans import ConanFile, tools, os
 
 class BoostFiberConan(ConanFile):
     name = "Boost.Fiber"
-    version = "1.64.0"
+    version = "1.65.1"
     generators = "boost" 
     settings = "os", "arch", "compiler", "build_type"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-fiber"
-    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["fiber"]
     options = {"shared": [True, False]}
     default_options = "shared=False"
     build_requires = "Boost.Generator/0.0.1@bincrafters/testing"
-    requires =  "Boost.Assert/1.64.0@bincrafters/testing", \
-                      "Boost.Config/1.64.0@bincrafters/testing", \
-                      "Boost.Context/1.64.0@bincrafters/testing", \
-                      "Boost.Core/1.64.0@bincrafters/testing", \
-                      "Boost.Intrusive/1.64.0@bincrafters/testing", \
-                      "Boost.Predef/1.64.0@bincrafters/testing", \
-                      "Boost.Smart_Ptr/1.64.0@bincrafters/testing"
+    requires =  "Boost.Assert/1.65.1@bincrafters/testing", \
+                      "Boost.Config/1.65.1@bincrafters/testing", \
+                      "Boost.Context/1.65.1@bincrafters/testing", \
+                      "Boost.Core/1.65.1@bincrafters/testing", \
+                      "Boost.Intrusive/1.65.1@bincrafters/testing", \
+                      "Boost.Predef/1.65.1@bincrafters/testing", \
+                      "Boost.Smart_Ptr/1.65.1@bincrafters/testing"
 
                       #assert1 config0 context12 core2 intrusive6 predef0 smart_ptr4
                       
