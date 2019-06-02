293ebc2b5c9051f56f91305ef4cbb8203eb2862d
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:11:25 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,26 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostFiberConan(base.BoostBaseConan):
     name = "boost_fiber"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_fiber"
-    lib_short_names = ["fiber"]
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
-    source_only_deps = [
-        "algorithm",
-        "format"
-    ]
-    b2_requires = [
-        "boost_assert",
-        "boost_config",
-        "boost_context",
-        "boost_core",
-        "boost_filesystem",
-        "boost_intrusive",
-        "boost_predef",
-        "boost_smart_ptr"
-    ]
+    version = "1.70.0"
